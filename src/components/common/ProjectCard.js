368589b6edcf9774201d385452d8c25126efc0a5
Md Shamim <5222250-os-shamim@users.noreply.gitlab.com>
2021-06-06 23:07:17 +0600

    project card improved

--- src/components/common/ProjectCard.js
+++ src/components/common/ProjectCard.js
@@ -1,10 +1,11 @@
-import React, {Fragment} from "react";
+import React from "react";
 import {Col} from "react-bootstrap";
 import "./project_card.scss";
+import {Link} from "react-router-dom";
 
-const ProjectCard = ({projectImage, projectTitle, projectDescription}) => {
+const ProjectCard = ({projectImage, projectTitle, projectDescription, workAt, contributions, workWith}) => {
     return (
-        <Fragment>
+        <>
             <Col lg={4} className="project-col">
                 <div className="project-card">
                     <img src={projectImage} alt="alt"/>
@@ -12,9 +13,29 @@ const ProjectCard = ({projectImage, projectTitle, projectDescription}) => {
                 <div className="project-card-details">
                     <h4>{projectTitle}</h4>
                     <p>{projectDescription}</p>
+                    <div className="contributes">
+                        <p>Contributes on</p>
+                        {
+                            contributions.map(v => <span key={v}>{v}</span>)
+                        }
+                    </div>
+                    <div className="work-with">
+                        <p>Work with</p>
+                        {
+                            workWith.map(v => <span key={v}>{v}</span>)
+                        }
+                    </div>
+                    <div className="company-section">
+                        <span className="label">At</span>
+                        {
+                            workAt.url ? <Link className="company" to={`//${workAt.url}`} target="_blank">
+                                {workAt.name}
+                            </Link> : <b>{workAt.name}</b>
+                        }
+                    </div>
                 </div>
             </Col>
-        </Fragment>
+        </>
     );
 }
 
--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -7,16 +7,28 @@ const Project = () => {
 
     const projects = [
         {
-            id: 1,
-            projectImage: "/images/project-image/Fembula.png",
-            projectTitle: "Multi Vendor Ecommerce",
-            projectDescription: "This is a multi vendor ecommerce that built with ReactJS."
-        },
-        {
             id: 2,
             projectImage: "/images/project-image/s1.png",
             projectTitle: "Admin Portal",
-            projectDescription: "This is a full functional admin portal that built with ReactJS, AntD, Spring Boot."
+            projectDescription: "This is a full functional admin portal for fabric base e-commerce  that built with ReactJS, AntD, Spring Boot.",
+            workAt: {
+                url: "https://zaagsys.com",
+                name: "Zaag System Ltd."
+            },
+            contributions: ["Backend", "Admin Portal"],
+            workWith: ["Spring Book", "MySQL", "ReactJS", "AntD"],
+        },
+        {
+            id: 1,
+            projectImage: "/images/project-image/Fembula.png",
+            projectTitle: "Multi Vendor Ecommerce",
+            projectDescription: "This is a multi vendor e-commerce for global product that built with ReactJS, NodeJS, MongoDB.",
+            workAt: {
+                url: null,
+                name: "Freelance."
+            },
+            contributions: ["Admin Portal"],
+            workWith: ["ReactJS", "AntD"],
         },
     ]
 
@@ -26,10 +38,14 @@ const Project = () => {
             <Row>
                 {
                     projects && projects.map(project =>
-                        <ProjectCard key={project.id}
-                                     projectImage={project.projectImage}
-                                     projectTitle={project.projectTitle}
-                                     projectDescription={project.projectDescription}
+                        <ProjectCard
+                            key={project.id}
+                            projectImage={project.projectImage}
+                            projectTitle={project.projectTitle}
+                            projectDescription={project.projectDescription}
+                            workAt={project.workAt}
+                            contributions={project.contributions}
+                            workWith={project.workWith}
                         />
                     )
                 }
